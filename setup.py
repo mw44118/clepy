ed67ae480ac847b86e9e15656a9e0e8904233ccd
W. Matthew Wilson <matt@tplus1.com>
2010-08-09 09:54:36 -0400

    bumped version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if sys.version_info < (2, 6, 0, '', 0):
 
 setup(
     name="clepy",
-    version="0.3.18",
+    version="0.3.19",
     packages=["clepy"],
     install_requires=dependencies,
     url="http://code.google.com/p/clepy",
